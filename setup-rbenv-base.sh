43d8310de221bda8806e145f13296b1e358acef7
Hiroshi Miura <miurahr@linux.com>
2015-04-11 10:07:19 +0900

    install ruby-build as rbenv plugins
    - remove build dependencies to reduce image size.
      now later step should install build dependencies by its own.
    
    Signed-off-by: Hiroshi Miura <miurahr@linux.com>

--- setup-rbenv-base.sh
+++ setup-rbenv-base.sh
@@ -4,35 +4,16 @@ set -e
 # How to use
 #
 #  You can use it by sourcing /etc/profile.d/rbenv.sh from your login shell.
-#
+#  and install ruby using ruby-build: rbenv install <version>
 
 # part 0: install rbenv and plugins
+# CAUTION: here is no Ruby installation
 
 ##########################################
 # dependencies
 #
 env DEBIAN_FRONTEND=noninteractive apt-get update
-env DEBIAN_FRONTEND=noninteractiv apt-get -y install \
-  build-essential \
-  curl \
-  unzip \
-  git-core \
-  gcc \
-  g++ \
-  make \
-  bison \
-  libgdbm-dev \
-  libncursesw5-dev \
-  libncurses5-dev \
-  libreadline6-dev \
-  tcl8.5-dev \
-  tk8.5-dev \
-  zlib1g-dev \
-  libssl-dev \
-  procps \
-  libffi-dev \
-  libyaml-dev \
-  chrpath
+env DEBIAN_FRONTEND=noninteractiv apt-get -y install git-core
 
 ##########################################
 # install rbenv
@@ -46,15 +27,6 @@ echo 'export PATH="$RBENV_ROOT/bin:$PATH"' >> /etc/profile.d/rbenv.sh
 echo 'eval "$(rbenv init -)"' >> /etc/profile.d/rbenv.sh
 
 chmod +x /etc/profile.d/rbenv.sh
-source /etc/profile.d/rbenv.sh
-
-##########################################
-# Install ruby-build:
-pushd /tmp
-  git clone git://github.com/sstephenson/ruby-build.git
-  cd ruby-build
-  ./install.sh
-popd
 
 ############################################
 # plugins
@@ -63,3 +35,7 @@ popd
 mkdir /opt/rbenv/plugins
 git clone git://github.com/dcarley/rbenv-sudo.git /opt/rbenv/plugins/rbenv-sudo
 
+# Install ruby-build as rbenv plugin
+git clone git://github.com/sstephenson/ruby-build.git /opt/rbenv/plugins/ruby-build
+
+exit 0
